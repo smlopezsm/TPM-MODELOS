05d242281fe878c8ebeb796aec964d898dffc04a
Santiago Lopez <santilm.sjlm@gmail.com>
2026-03-31 12:16:36 -0300

    creando Pista

--- main/src/Recursos/Pista.java
+++ main/src/Recursos/Pista.java
@@ -1,5 +1,41 @@
 package Recursos;
 
+import java.util.LinkedList;
+import java.util.Queue;
+import Entidades.Avion;
+
 public class Pista {
+    private int id;
+    private boolean ocupada;
+    private Queue<Avion> colaDeEspera;
+
+    public Pista(int id) {
+        this.id = id;
+        this.ocupada = false;
+        this.colaDeEspera = new LinkedList<>();
+    }
+
+    public boolean isOcupada() {
+        return ocupada;
+    }
+
+    public void setOcupada(boolean ocupada) {
+        this.ocupada = ocupada;
+    }
+
+    public void agregarACola(Avion avion) {
+        colaDeEspera.add(avion);
+    }
+
+    public Avion sacarDeCola() {
+        return colaDeEspera.poll();
+    }
+
+    public boolean hayCola() {
+        return !colaDeEspera.isEmpty();
+    }
     
+    public int getTamañoCola() {
+        return colaDeEspera.size();
+    }
 }
